7dfc8e1ca56f42b81d3afacc715171140008ac83
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2026-03-10 17:45:43 +0530

    redis host changed

--- src/queue/judge.queue.ts
+++ src/queue/judge.queue.ts
@@ -3,9 +3,9 @@ import Redis from "ioredis";
 
 //@ts-ignore
 export const connection = new Redis({
-  host: "localhost",
+  host: process.env.REDIS_HOST || "redis",
   port: 6379,
-   maxRetriesPerRequest: null
+  maxRetriesPerRequest: null
 });
 
 export const judgeQueue = new Queue("judge-queue", {
