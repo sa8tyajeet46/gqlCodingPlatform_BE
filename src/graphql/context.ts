863951fb6a16807c3f6565f9ece0b7816de0e035
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2026-03-10 10:37:25 +0530

    prisma fixes

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,7 +1,7 @@
 // graphql/context.ts
 import config from "../config/config.js";
 import { prisma } from "../config/prisma.js";
-import type { PrismaClient } from '../generated/prisma/index.js';
+import type { PrismaClient } from '@prisma/client';
 import type { ExpressContextFunctionArgument } from '@as-integrations/express5';
 import jwt from "jsonwebtoken";
 import { z } from "zod";
